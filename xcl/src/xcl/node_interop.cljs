04a6f866a4d89f9667505d246b317e754b123cab
natto <whacked@users.noreply.github.com>
2018-12-16 00:38:09 -0800

    Add tokens -> excerpt finder for pdf / epub

--- xcl/src/xcl/node_interop.cljs
+++ xcl/src/xcl/node_interop.cljs
@@ -1,11 +1,13 @@
 (ns xcl.node-interop
-  (:require [xcl.core :as sc]
+  (:require [xcl.common :refer [get-file-extension]]
+            [xcl.core :as sc]
             [xcl.content-interop :as ci]
             [xcl.core :refer [render-transclusion]]
             [xcl.external :as ext]
             [xcl.pdfjslib-interop
              :refer [pdfjslib-load-text
-                     set-pdfjslib!]]
+                     set-pdfjslib!]
+             :as pdfi]
             ["pdfjs-dist" :as pdfjsLib]
             [xcl.node-epub-interop :as epubi]))
 
@@ -57,3 +59,49 @@
                 (postprocessor-fn content (clj->js xcl-spec) depth))))
        (apply render-transclusion
               candidate-seq-loader content-loader source-text)))
+
+(defn get-exact-text-from-partial-string-match
+  [file-path match-string on-complete]
+  (let [search-tokens (clojure.string/split match-string #"\s+")
+        result (atom nil)]
+   (case (get-file-extension file-path)
+     "epub" (epubi/load-and-get-text
+             file-path
+             nil
+             nil
+             (fn on-get-section [{:keys [section chapter text]}]
+               (let [maybe-matches (sc/find-successive-tokens-in-content
+                                    text search-tokens)]
+                 (when (= (count maybe-matches)
+                          (count search-tokens))
+                   (let [first-match (first maybe-matches)
+                         last-match (last maybe-matches)
+                         index-beg (:index first-match)
+                         index-end (+ (:index last-match)
+                                      (:length last-match))]
+                     (swap! result assoc
+                            :section section
+                            :chapter chapter
+                            :excerpt (subs text index-beg index-end))))))
+             (fn on-complete-all-sections [_sections]
+               (on-complete @result)))
+     
+     "pdf" (pdfi/process-pdf
+            file-path
+            (fn on-get-page-text [page-num page-text]
+              (let [maybe-matches (sc/find-most-compact-token-matches-in-content
+                                   page-text search-tokens)]
+                (when (= (count maybe-matches)
+                         (count search-tokens))
+                  (let [first-match (first maybe-matches)
+                        last-match (last maybe-matches)
+                        index-beg (:index first-match)
+                        index-end (+ (:index last-match)
+                                     (:length last-match))]
+                    (swap! result assoc
+                           :page page-num
+                           :excerpt (subs page-text index-beg index-end))))))
+            (fn on-complete-page-texts [page-texts]
+              (on-complete @result)))
+     
+     nil)))
