81218479cbbc111b2a69e1030ed0adfeaa34a447
natto <whacked@users.noreply.github.com>
2019-01-04 00:04:30 -0800

    Add assorted resolution examples to node console test

--- xcl/src/xcl/test.cljs
+++ xcl/src/xcl/test.cljs
@@ -1,28 +1,54 @@
 (ns xcl.test
   (:require ["sqlite3" :as sqlite3]
             ["yesql" :as yesql]
+            ["fs" :as fs]
             [xcl.core :as sc]
+            [xcl.content-interop :as ci]
+            [xcl.node-interop :as ni]
             [xcl.node-common :refer
              [path-join]]
             [xcl.external :as ext]
             [xcl.calibre-interop :as calibre]
             [xcl.zotero-interop :as zotero]))
 
-(def all-tests (atom 0))
+(def all-tests (atom []))
+
+(defn nocolor [& ss]
+  (str (apply str ss) "\033[0m"))
+
+(defn red [s]
+  (js/console.log (nocolor "\033[31m" s)))
+(defn green [s]
+  (js/console.log (nocolor "\033[32m" s)))
+(defn yellow [s]
+  (js/console.log (nocolor "\033[33m" s)))
+(defn blue [s]
+  (js/console.log (nocolor "\033[34m" s)))
+(defn magenta [s]
+  (js/console.log (nocolor "\033[36m" s)))
+(defn cyan [s]
+  (js/console.log (nocolor "\033[36m" s)))
+
+(defn run-all-tests! []
+  (when-let [test-func (first @all-tests)]
+    (swap! all-tests rest)
+    (test-func)))
 
 (defn signal-test-done! []
-  (swap! all-tests dec)
-  (when (<= @all-tests 0)
-    (js/process.exit)))
+  (red
+   (str (count @all-tests) " tests remaining..."))
+  (if (<= (count @all-tests) 0)
+    (js/process.exit)
+    (run-all-tests!)))
 
 (defn zotero-test []
   (zotero/load-pdf
    "Trace-based just-in-time*.pdf"
    "Monkey observes that so TraceMonkey attempts"
    (fn [page text]
+     (green "[zotero test OK]")
      (js/console.info
-      (str "[zotero test OK]\n"
-           "    page: " page "\n"
+      (str "    page: " page "\n"
            "    "
            text
            "\n\n"))
@@ -36,6 +62,7 @@
    "QuIcK sTaRt*.epub"
    "Calibre display possible matches for entered"
    (fn [text]
+     (green "[calibre OK]")
      (js/console.log
       (str "    calibre epub: " (count text) " bytes\n\n"))
      (signal-test-done!))))
@@ -47,8 +74,8 @@
     (external-loader
      resource-spec
      (fn [text]
-       (js/console.log (str "[PDF OK]\n"
-                            "    " (pr-str resource-spec) "\n"
+       (green "[PDF OK]")
+       (js/console.log (str "    " (pr-str resource-spec) "\n"
                             "    " (-> text
                                        (clojure.string/trim)
                                        (subs 0 200))
@@ -62,8 +89,8 @@
     (external-loader
      resource-spec
      (fn [text]
-       (js/console.log (str "[EPUB OK]\n"
-                            "    " (pr-str resource-spec) "\n"
+       (green "[EPUB OK]")
+       (js/console.log (str "    " (pr-str resource-spec) "\n"
                             "    " (-> text
                                        (clojure.string/trim)
                                        (subs 0 200))
@@ -71,8 +98,9 @@
        (signal-test-done!)))))
 
 (defn add-node-test! [test-func]
-  (swap! all-tests inc)
-  (test-func))
+  (cyan ">>>>>>>>> adding a test!")
+  (swap! all-tests conj test-func)
+  (blue (str " ----- added a test; now " (count @all-tests))))
 
 (defn -main []
   (js/console.log
@@ -81,12 +109,94 @@
         "### process running from dir\n"
         "    " (.cwd js/process)
         "\n\n"))
+
+  (def XCL-SERVER-RESOURCE-BASE-DIR
+    (path-join
+     (.cwd js/process) ".."))
+
+  (defn get-resource-path
+    [file-name]
+    (path-join
+     XCL-SERVER-RESOURCE-BASE-DIR
+     file-name))
+
+  (defn slurp [path]
+    (.readFileSync fs path "utf-8"))
+
+  (defn load-from-directive [directive]
+    (let [spec (sc/parse-link directive)
+          full-content (slurp
+                        (get-resource-path
+                         (:resource-resolver-path spec)))]
+      (yellow
+       (str "*** SPEC ***\n"
+            (:resource-resolver-path spec) "\n"
+            (pr-str spec)))
+      (green
+       (str "\n"
+            "========================================"))
+      (println (ci/resolve-content
+                spec full-content))
+      (green "----------------------------------------")))
   
+  (add-node-test!
+   (fn default-file []
+     (let [directive "transcluding-org-elements.org"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn fs-file []
+     (let [directive "LICENSE::2-10"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn fs-file-with-line-range []
+     (let [directive "file:transcluding-org-elements.org::153,185"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn fs-file-with-org-heading []
+     (let [directive "file:README.org::*line range"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn fs-file-with-token-bound []
+     (let [directive "file:README.org::There is 1 goal...a range resource"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn xcl-first-token-range []
+     (let [directive "xcl:README.org?s=There is 1 goal...a range resource"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
+  (add-node-test!
+   (fn xcl-first-matching-paragraph []
+     (let [directive "xcl:README.org?para=the+pipe+is+an+obvious"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+  
+  (add-node-test!
+   (fn xcl-yaml-jsonpath []
+     (let [directive "xcl:xcl/public/test-highlight-file.yml?jsonpath=$.highlights[2].highlightText"]
+       (load-from-directive directive)
+       (signal-test-done!))))
+
   (when zotero/$zotero-library-directory
     (add-node-test! zotero-test))
+  
   (when calibre/$calibre-library-directory
     (add-node-test! calibre-test))
+  
   (add-node-test! pdf-loader-test)
-  (add-node-test! epub-loader-test))
+  
+  (add-node-test! epub-loader-test)
+  
+  (run-all-tests!))
 
 (-main)
