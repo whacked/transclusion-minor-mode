cffac6d6efecfcab2fcdad64892859133bbc835e
natto <whacked@users.noreply.github.com>
2019-01-16 23:30:49 -0800

    Add package-lint for emacs, reindent

--- default.nix
+++ default.nix
@@ -21,17 +21,20 @@ stdenv.mkDerivation rec {
         eval_forms="$eval_forms (package-initialize)"
         eval_forms="$eval_forms (add-to-list 'package-archives '(\\\"melpa\\\" . \\\"https://melpa.org/packages/\\\") t)"
         eval_forms="$eval_forms (add-to-list 'load-path \\\"~/.emacs.d/json-rpc-request.el\\\")"
-		alias emacs="emacs --eval=\"(progn $eval_forms)\""
+        alias emacs="emacs --eval=\"(progn $eval_forms)\""
 
         if [ ! -e $HOME ]; then
             echo "creating .nix directory"
             mkdir -p $HOME
 
-			emacs -batch --eval="(progn (package-initialize) (package-refresh-contents) (package-install 'ov) (package-install 'json-rpc))"
+            # dependencies
+            emacs -batch --eval="(progn (package-initialize) (package-refresh-contents) (package-install 'ov) (package-install 'json-rpc))"
+            # for best practices
+            emacs -batch --eval="(progn (package-initialize) (package-refresh-contents) (package-install 'package-lint))"
         fi
 
         if [ ! -e $HOME/.emacs.d/json-rpc-request.el ]; then
-			git clone https://github.com/whacked/json-rpc-request.el .nix/.emacs.d/json-rpc-request.el
+            git clone https://github.com/whacked/json-rpc-request.el .nix/.emacs.d/json-rpc-request.el
         fi
     '';
 }
