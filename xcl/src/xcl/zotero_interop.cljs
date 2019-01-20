a6979d71c89e96792179bd80b6d9869e92ad845b
natto <whacked@users.noreply.github.com>
2019-01-20 12:48:09 -0800

    Add html handler for zotero, change text proc to dispatcher
    previously assumes all zotero queries are for pdf.
    
    this change uses extension to dispatch for html or pdf

--- xcl/src/xcl/zotero_interop.cljs
+++ xcl/src/xcl/zotero_interop.cljs
@@ -1,11 +1,15 @@
 (ns xcl.zotero-interop
   (:require ["sqlite3" :as sqlite3]
+            ["fs" :as fs]
+            [xcl.common :refer
+             [get-file-extension]]
             [xcl.node-common :refer
              [path-exists? path-join]]
             [xcl.core :as sc]
             [xcl.content-interop :as ci]
             [xcl.sqlite :as sqlite]
             [xcl.external :as ext]
+            [xcl.dom-processing :as domp]
             [xcl.pdfjslib-interop :as pdfjslib]))
 
 (def $zotero-library-directory
@@ -58,66 +62,97 @@
              (let [{:keys [attachmentKey
                            attachmentPath]} (first rows)
                    file-path (zotero-get-attachment-key-path-filepath
-                              attachmentKey attachmentPath)
-                   ]
-               (if-not (path-exists? file-path)
+                              attachmentKey attachmentPath)]
+
+               (if (path-exists? file-path)
+                 (on-found-file file-path)
                  (if on-not-exist
                    (on-not-exist file-path)
                    (js/console.error
-                    (str "FILE AT [" file-path "] DOES NOT EXIST")))
-                 (on-found-file file-path)))))))))
+                    (str "FILE AT [" file-path "] DOES NOT EXIST")))))))))))
 
 (defn load-text-from-html
-  []
-  (throw (js/Error. "TODO")))
+  [file-path
+   target-string-or-spec
+   on-found-text
+   & [on-error]]
+  (let [xhtml-string (.readFileSync fs file-path "utf-8")
+        text-content (-> xhtml-string
+                         (domp/xhtml-string->text))]
+    (if (string? target-string-or-spec)
+      (let [tokens (clojure.string/split
+                    target-string-or-spec #"\s+")]
+        (on-found-text
+         (sc/get-resolved-substring-from-tokens
+          text-content tokens)))
+      (when-let [resolved-text (ci/resolve-content
+                                target-string-or-spec
+                                text-content)]
+        (js/console.info
+         (str "[zotero RANGE LOAD]\n"
+              (pr-str target-string-or-spec)))
+        (on-found-text resolved-text)))))
 
 (defn load-text-from-pdf
-  [pdf-search-string
+  [file-path
+   target-string-or-spec
+   on-found-text
+   & [on-error]]
+  (pdfjslib/process-pdf
+   file-path
+   (fn on-get-page-text [page-number text]
+     {:page page-number
+      :text text})
+   (fn on-complete-page-texts [pagetexts]
+     (let [_page-number-with-string
+           (some->> pagetexts
+                    (filter
+                     (fn [{:keys [page text]}]
+                       (if (string? target-string-or-spec)
+                         ;; find tokens directly
+                         (let [tokens (clojure.string/split
+                                       target-string-or-spec #"\s+")]
+                           (on-found-text
+                            (sc/get-resolved-substring-from-tokens
+                             text tokens)
+                            page))
+
+                         ;; use specification based resolution
+                         (when-let [resolved-text (ci/resolve-content
+                                                   target-string-or-spec
+                                                   text)]
+                           (js/console.info
+                            (str "[zotero RANGE LOAD]\n"
+                                 "    page: " page "\n"
+                                 "    "
+                                 (pr-str target-string-or-spec)))
+                           (on-found-text resolved-text page)))))
+                    (first)
+                    (:page))]
+       _page-number-with-string))
+   (or on-error identity)))
+
+(defn load-text-from-file
+  [file-search-string
    target-string-or-spec
    on-found-text
    & [on-error]]
   (find-matching-file
-   pdf-search-string
+   file-search-string
    (fn [file-path]
-     (pdfjslib/process-pdf
-      file-path
-      (fn on-get-page-text [page-number text]
-        {:page page-number
-         :text text})
-      (fn on-complete-page-texts [pagetexts]
-        (let [_page-number-with-string
-              (some->> pagetexts
-                       (filter
-                        (fn [{:keys [page text]}]
-                          (if (string? target-string-or-spec)
-                            ;; find tokens directly
-                            (let [tokens (clojure.string/split
-                                          target-string-or-spec #"\s+")
-                                  maybe-matches
-                                  (sc/find-successive-tokens-in-content
-                                   text tokens)]
-                              (when (= (count maybe-matches)
-                                       (count tokens))
-                                (let [match-text
-                                      (subs
-                                       text
-                                       (:index (first maybe-matches))
-                                       (+ (:index (last maybe-matches))
-                                          (:length (last maybe-matches))))]
-                                  (on-found-text page match-text))
-                                page))
-
-                            ;; use specification based resolution
-                            (when-let [resolved-text (ci/resolve-content
-                                                      target-string-or-spec
-                                                      text)]
-                              (js/console.info
-                               (str "[zotero RANGE TEST OK]\n"
-                                    "    page: " page "\n"
-                                    "    "
-                                    (pr-str target-string-or-spec)))
-                              (on-found-text page resolved-text)))))
-                       (first)
-                       (:page))]
-          _page-number-with-string))
-      (or on-error identity)))))
+     (let [extension (get-file-extension file-path)
+           handler (case extension
+                     "pdf" load-text-from-pdf
+                     "html" load-text-from-html
+                     nil)]
+       (if handler
+         (find-matching-file
+          file-search-string
+          (fn [file-path]
+            (handler
+             file-path target-string-or-spec
+             on-found-text on-error)))
+         (do
+           (js/console.warn
+            (str "[zotero WARNING] unhandled extension: " extension))
+           (on-found-text file-path)))))))
