3542ee42e08c3237e6c6ab216d7d34e0980c2df0
natto <whacked@users.noreply.github.com>
2019-01-21 11:36:38 -0800

    Remove superseded freex code; normalize namespace

--- xcl-transclude.el
+++ xcl-transclude.el
@@ -194,306 +194,76 @@
 
 ;; ref http://emacs.stackexchange.com/a/358
 (defvar xcl-transclude-mode-map (make-sparse-keymap)
-  "Keymap while transclude-mode is active.")
+  "Keymap while xcl-transclude-mode is active.")
 
 ;; ref http://emacs.stackexchange.com/questions/352/how-to-override-major-mode-bindings
 ;; ref http://nullprogram.com/blog/2013/02/06/
 ;;;###autoload
-(define-minor-mode transclude-mode
+(define-minor-mode xcl-transclude-mode
   "A minor mode so that my key settings override annoying major modes."
   nil
-  :lighter "transclude-mode"
+  :lighter "xcl-transclude-mode"
   xcl-transclude-mode-map)
 
 ;; Source: http://stackoverflow.com/questions/683425/globally-override-key-binding-in-emacs
 (defadvice load (after give-my-keybindings-priority)
   "Try to ensure that my keybindings always have priority."
-  (if (not (eq (car (car minor-mode-map-alist)) 'transclude-mode))
-      (let ((mykeys (assq 'transclude-mode minor-mode-map-alist)))
-        (assq-delete-all 'transclude-mode minor-mode-map-alist)
+  (if (not (eq (car (car minor-mode-map-alist)) 'xcl-transclude-mode))
+      (let ((mykeys (assq 'xcl-transclude-mode minor-mode-map-alist)))
+        (assq-delete-all 'xcl-transclude-mode minor-mode-map-alist)
         (add-to-list 'minor-mode-map-alist mykeys))))
 (ad-activate 'load)
 
-;;;###autoload
-(define-globalized-minor-mode global-transclude-mode transclude-mode turn-on-transclude-mode)
+(define-globalized-minor-mode global-xcl-transclude-mode xcl-transclude-mode turn-on-xcl-transclude-mode)
 
 ;; Turn off the minor mode in the minibuffer
-(add-hook 'minibuffer-setup-hook 'turn-off-transclude-mode)
+(add-hook 'minibuffer-setup-hook 'turn-off-xcl-transclude-mode)
 
 ;;;###autoload
-(defun turn-on-transclude-mode ()
-  "Turns on my-mode."
+(defun turn-on-xcl-transclude-mode ()
+  "Turns on xcl-transclude-mode."
   (interactive)
-  (transclude-mode t))
+  (xcl-transclude-mode t))
 
 ;;;###autoload
-(defun turn-off-transclude-mode ()
-  "Turns off my-mode."
+(defun turn-off-xcl-transclude-mode ()
+  "Turns off xcl-transclude-mode."
   (interactive)
-  (transclude-mode -1))
+  (xcl-transclude-mode -1))
 
-(defun sweep-and-refresh-matching-overlay (source-filepath)
-  (let ((check-buffer-list (buffer-list)))
-    (while check-buffer-list
-      (if (and (buffer-file-name (car check-buffer-list))
-               (string= source-filepath
-                        (expand-file-name
-                         (buffer-file-name (car check-buffer-list)))))
-          (let ((now-buf (current-buffer)))
-            (set-buffer (car check-buffer-list))
-            (revert-buffer nil t)))
-      (setq check-buffer-list (cdr check-buffer-list)))))
-
-(setq freex-active-source-file-list nil)
-(make-variable-buffer-local 'freex-active-source-file-list)
-;; force-set
-;; (setq-local freex-active-source-file-list (list "/path/to/file.org"))
-
-(setq freex-color-base "#777777")
-(setq freex-color-modified "#993333")
-
-(defun freex-get-overlay-at-point ()
+;;;###autoload
+(defun xcl-transclude--get-overlay-at-point ()
   (interactive)
   (let ((ov-list (overlays-at (point)))
         rtn)
     (while ov-list
       (let ((ov (car ov-list)))
-        (if (overlay-get ov 'is-freex-embed)
+        (if (overlay-get ov 'is-xcl-transclude-overlay)
             (setq rtn ov)))
       (setq ov-list (cdr ov-list)))
     rtn))
 
-(defun freex-close-overlay (pos)
-  (interactive (list (point)))
-  (let ((ov-list (overlays-at pos)))
-    (while ov-list
-      (let ((ov (car ov-list))
-            (current-modified-p (buffer-modified-p)))
-        (if (and ov-list (overlay-get ov 'is-freex-embed))
-            (let ((source-filepath (overlay-get ov 'full-filename)))
-              (if (or (null (overlay-get ov 'is-modified))
-                      (yes-or-no-p "source file for this overlay was modified. discard changes? "))
-                  (let ((del-start (overlay-start ov))
-                        (del-end (overlay-end ov)))
-                    (remove-overlays 1 (buffer-size) 'full-filename source-filepath)
-                    (setq-local freex-active-source-file-list
-                                (delete source-filepath
-                                        (buffer-local-value 'freex-active-source-file-list (current-buffer))))
-                    (kill-region del-start del-end)
-                    (set-buffer-modified-p current-modified-p))))))
-      (setq ov-list (cdr ov-list)))))
-
-
-(defun freex-overlay-set-modified-status (ov is-modified?)
+;; to deprecate
+(defun xcl-transclude--overlay-set-modified-status (ov is-modified?)
   (if is-modified?
       (progn (overlay-put ov 'is-modified t)
              (overlay-put ov 'face
-                          (cons 'background-color freex-color-modified)))
+                          (cons 'background-color xcl-transclude--overlay-color-modified)))
     (progn (overlay-put ov 'is-modified nil)
            (overlay-put ov 'face
-                        (cons 'background-color freex-color-base)))))
+                        (cons 'background-color xcl-transclude--overlay-color-base)))))
 
-(defun get-text-between-2-line-index (start-line until-line)
-  "until IS included! this is for the human-friendly specification
-   by 'lines A to B' or L20~24 etc., thus it is inclusive"
-  (let (start-pos end-pos)
-    (save-excursion
-      (setq start-pos
-            (if (null start-line)
-                0
-              (progn
-                (goto-char (point-min))
-                (forward-line (1- start-line))
-                (point))))
-      (setq end-pos
-            (if (null until-line)
-                (buffer-size)
-              (progn
-                (goto-char (point-min))
-                (forward-line until-line)
-                (point)))))
-    (buffer-substring-no-properties start-pos end-pos)))
-
-;; note: we assume there will only be 1 overlay at a time!
-;; so we only check `car`
-(defun check-overlay-and-save (&optional whatever)
-  (interactive)
-  ;; (say "保存しましょう4")
-  
-  (let ((overlays (overlays-at (point)))
-        freex-ov)
-    ;; fish out the freex overlay if any
-    (while overlays
-      (let ((ov (car overlays)))
-        (if (overlay-get ov 'is-freex-embed)
-            (setq freex-ov ov)))
-      (setq overlays (cdr overlays)))
-    (if (and freex-ov
-             (overlay-get freex-ov 'is-freex-embed)
-             (overlay-get freex-ov 'is-modified))
-        (let ((overlay-text (buffer-substring (overlay-start freex-ov)
-                                              (overlay-end freex-ov)))
-              (source-filepath (overlay-get freex-ov 'full-filename))
-              (current-modified-p (buffer-modified-p)))
-          (with-temp-buffer
-            (insert overlay-text)
-            (write-file source-filepath nil))
-          
-          ;; TODO
-          ;; upon opening a new overlay
-          ;; loop in all current open files
-          ;; if file is same as overlay source file
-          ;; add save hook so that
-          ;; [X] on save of that source file, overlay is updated
-          ;;     --> should be handled by freex-update-active-overlay-list()
-          ;; [X] on save of overlay, source file is updated
-          (sweep-and-refresh-matching-overlay source-filepath)
-          
-          ;; force overlay to be not modified
-          (freex-overlay-set-modified-status freex-ov nil)
-          (set-buffer-modified-p current-modified-p)
-          (message (format "saved %s" (overlay-get freex-ov 'full-filename))))
-      (let ((current-filepath (expand-file-name (buffer-file-name (current-buffer)))))
-        ;; "normal" save
-        ;; go through all current overlays, exclude all freex overlays,
-        ;; and write out the resulting text (sans freex overlay texts)
-        (let ((cur-buf (current-buffer))
-              (max-idx (point-max)))
-          (let ((ov-list (overlays-in 0 (buffer-size)))
-                exclude-list)
-            ;; compile list of indexes from freex overlays
-            (while ov-list
-              (let ((ov (car ov-list)))
-                (if (overlay-get ov 'is-freex-embed)
-                    (let ((ov-start (overlay-start ov))
-                          (ov-end   (overlay-end ov)))
-                      (if (< ov-start ov-end)
-                          (setq exclude-list (cons (list ov-start ov-end)
-                                                   exclude-list))))))
-              (setq ov-list (cdr ov-list)))
-            (message (format "***** exclude list %s\n" exclude-list))
-            
-            ;; loop through sorted idx list, and only insert
-            ;; text that is not within them
-            (let ((start-idx 1)
-                  (sorted-idx-list (sort exclude-list #'(lambda (a b) (< (first a) (first b))))))
-              
-              (with-temp-buffer
-                (while sorted-idx-list
-                  (let ((idx-pair (car sorted-idx-list)))
-                    (insert (with-current-buffer cur-buf
-                              (buffer-substring start-idx (first idx-pair))))
-                    (setq start-idx (second idx-pair))
-                    (setq sorted-idx-list (cdr sorted-idx-list))))
-                (if (< start-idx max-idx)
-                    (insert (with-current-buffer cur-buf
-                              (buffer-substring start-idx max-idx))))
-                (write-file current-filepath)))))
-
-        ;; XXX WARNING this is potentially dangerous if there are
-        ;; concurrent writes or multiple windows of the same file
-        ;; open. i have not thought through this.
-        (set-buffer-modified-p nil)
-        (clear-visited-file-modtime)
-        
-        )
-      ))
-  )
-
-(defun overlay-mark-as-modified
+;; to deprecate
+(defun xcl-transclude--overlay-mark-as-modified
+    ;; see https://github.com/gregdetre/emacs-freex/blob/master/freex-embed.el#L343
     (overlay is-post start end &optional replaced-length)
   "This is the function that gets called by the overlay's
   modified hook. Must have these arguments."
   (let ((current-modified-p (buffer-modified-p)))
     (when is-post
-      (freex-overlay-set-modified-status overlay t)
+      (xcl-transclude--overlay-set-modified-status overlay t)
       (set-buffer-modified-p current-modified-p))))
 
-;; TODO
-;; [X] make line-start and line-end work (prelim)
-(defun freex-create-overlay
-    (beg source-filepath line-start line-end)
-  (let ((properties (list 'full-filename source-filepath
-                          'line-start 1
-                          'line-end nil)))
-    "simplified from freex-embed-create-overlay, but removed
-     insert-funct, save-funct.
-
-     create an overlay embed at point `beg` using contents
-     of `source-filepath`
-     "
-    (let ((ov nil)
-          ;; pre-read the source text, instead of using e.g.
-          ;; (insert-file), because without knowing the
-          ;; length of the text, we either resort to
-          ;; creating a rear-advance overlay by calling
-          ;; (make-overlay beg beg nil nil t) -- which is
-          ;; what freex did -- else we get strange overlay
-          ;; growing/shrinking effects when editing around
-          ;; the edges
-          (source-text (with-temp-buffer
-                         (insert-file-contents source-filepath)
-                         (let ((pt-start (progn
-                                           (goto-line (or (null line-start) 1))
-                                           (point)))
-                               (pt-end   (progn
-                                           (if (null line-end)
-                                               (end-of-buffer)
-                                             (goto-line (1+ line-end)))
-                                           (point))))
-                           (buffer-substring pt-start pt-end))))
-          (modified-p (buffer-modified-p)))
-      
-      ;; add any optional properties to it, unless the
-      ;; PROPERTIES list has an even number of items
-      (unless (equal (mod (length properties) 2) 0)
-        (error "Properties list must be even in length"))
-
-      (save-excursion
-        ;; make the overlay consist of a single newline, to
-        ;; begin with
-        (goto-char beg)
-
-        (insert source-text)
-        
-        ;; start end
-        ;; buffer (current-buffer)
-        ;; front-advance nil
-        ;; rear-advance nil
-        (setq ov (make-overlay beg (+ beg (length source-text))))
-        ;; add the default properties
-        (overlay-put ov 'is-freex-embed t)
-        (overlay-put ov 'evaporate nil)
-        (overlay-put ov 'rear-nonsticky t)
-        ;; set the color of the text
-        (overlay-put ov 'face
-                     (cons 'background-color
-                           freex-color-base))
-        (while properties
-          (overlay-put ov (pop properties) (pop properties)))
-
-        ;; add the modification hooks (after we have inserted)
-        (overlay-put ov 'modification-hooks
-                     '(overlay-mark-as-modified))
-        ;; set buffer modified to nil if it was not already modified
-        (set-buffer-modified-p modified-p)
-        ;; force overlay to be not modified
-        (freex-overlay-set-modified-status ov nil)
-
-        (setq-local freex-active-source-file-list
-                    (cons
-                     source-filepath
-                     (buffer-local-value 'freex-active-source-file-list (current-buffer))
-                     ))
-        ov))))
-
-(defun nonzero-or-nil (s)
-  (let ((num (string-to-number s)))
-    (cond ((= 0 num)
-           nil)
-          (t num))))
-
-(define-key xcl-transclude-mode-map (kbd "C-x C-s") 'check-overlay-and-save)
 
 (provide 'xcl-transclude)
 ;;; xcl-transclude.el ends here
