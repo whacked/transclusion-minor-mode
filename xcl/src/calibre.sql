4f4af5a7d17aa43ec4454a9325208b4c2b2780c6
whacked <whacked@users.noreply.github.com>
2020-06-28 00:44:12 -0700

    restrict calibre search SQL to EPUB

--- xcl/src/calibre.sql
+++ xcl/src/calibre.sql
@@ -11,6 +11,7 @@ SELECT
 FROM data AS d
   LEFT OUTER JOIN books AS b ON d.book = b.id
 WHERE
-   lower(b.author_sort) LIKE :book_author_sort_LIKE
-OR lower(b.title) LIKE :book_title_LIKE
+   (lower(b.author_sort) LIKE :book_author_sort_LIKE
+    OR lower(b.title) LIKE :book_title_LIKE)
+AND upper(d.format) = 'EPUB'
 LIMIT :limit
